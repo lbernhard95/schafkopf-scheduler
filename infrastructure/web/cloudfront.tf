7bbbf7bee39b04280c7f69a957e19c606f0086e9
lukas.bernhard <lukas.bernhard@konux.de>
2024-11-16 14:14:53 +0100

    also server for 403

--- infrastructure/web/cloudfront.tf
+++ infrastructure/web/cloudfront.tf
@@ -19,8 +19,14 @@ resource "aws_cloudfront_distribution" "web" {
   default_root_object = "index.html"
 
   custom_error_response {
-    error_code = 404
-    response_code = 200
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
     response_page_path = "/index.html"
   }
 
