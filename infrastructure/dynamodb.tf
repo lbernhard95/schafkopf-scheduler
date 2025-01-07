cb751a7ff7733ba0256491cfa3869805e9f73b5d
lukas.bernhard <lukas.bernhard@konux.de>
2025-01-07 21:41:49 +0100

    create new dynamodb table

--- infrastructure/dynamodb.tf
+++ infrastructure/dynamodb.tf
@@ -19,3 +19,20 @@ resource "aws_dynamodb_table" "polls" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table" "polls" {
+  name         = "schafkopf_scheduler"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "partition_key"
+  range_key    = "sort_key"
+
+  attribute {
+    name = "partition_key"
+    type = "S"
+  }
+
+  attribute {
+    name = "sort_key"
+    type = "S"
+  }
+}
