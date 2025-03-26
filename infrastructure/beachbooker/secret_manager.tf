144760dd2c19454998df30c8e3690892d50b35ec
lukas.bernhard <lukas.bernhard@konux.de>
2025-03-26 23:28:56 +0100

    remove codeowners

--- infrastructure/beachbooker/secret_manager.tf
+++ infrastructure/beachbooker/secret_manager.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "zhs_user_secret" {
-  name = "zhs_user_secret_2"
+  name = "zhs_user_secret_3"
 }
 
 data "aws_secretsmanager_secret_version" "zhs_user_secret" {
