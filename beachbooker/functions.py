ecc700578614d97ec3928605846fb0910cd8ff6f
lukas.bernhard <lukas.bernhard@konux.de>
2025-04-13 10:27:47 +0200

    scale task

--- beachbooker/functions.py
+++ beachbooker/functions.py
@@ -16,7 +16,7 @@ def get_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
-
+    chrome_options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
