c6b0bb006e8c082d09a6ec348e040d82b5017346
lukas.bernhard <lukas.bernhard@konux.de>
2025-12-07 20:56:57 +0100

    connect ebg scanner

--- infrastructure/ebg_scanner/cloudwatch.tf
+++ infrastructure/ebg_scanner/cloudwatch.tf
@@ -7,4 +7,5 @@ resource "aws_cloudwatch_log_group" "ebg_scanner_logs" {
 resource "aws_cloudwatch_event_rule" "ebg_scanner" {
   name                = "ebg_scanner"
   schedule_expression = "cron(0 12,19 * * ? *)" # Triggers every day at 12 PM and 7 PM UTC
+  arn                 = aws_lambda_function.ebg_scanner.arn
 }
