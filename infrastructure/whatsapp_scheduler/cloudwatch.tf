23327e29bc1ae5c8c72efd6296360c3a8c31bf44
lukas.bernhard <lukas.bernhard@konux.de>
2026-03-07 11:08:33 +0100

    run every two weeks

--- infrastructure/whatsapp_scheduler/cloudwatch.tf
+++ infrastructure/whatsapp_scheduler/cloudwatch.tf
@@ -1,16 +1,20 @@
 resource "aws_cloudwatch_log_group" "whatsapp_scheduler" {
-  name              = "/aws/ecs/whatsapp_scheduler"
+  name              = "/aws/lambda/whatsapp_scheduler"
   retention_in_days = 30
 }
 
+resource "aws_scheduler_schedule" "whatsapp_scheduler" {
+  name                         = "whatsapp_scheduler"
+  schedule_expression          = "rate(14 days)"
+  schedule_expression_timezone = "Europe/Berlin"
+  start_date                   = "2026-03-07T10:00:00+01:00"
 
-resource "aws_cloudwatch_event_rule" "whatsapp_scheduler" {
-  name                = "whatsapp_scheduler"
-  schedule_expression = "rate(14 days)"
-}
+  flexible_time_window {
+    mode = "OFF"
+  }
 
-resource "aws_cloudwatch_event_target" "lambda_target" {
-  rule      = aws_cloudwatch_event_rule.whatsapp_scheduler.name
-  target_id = "MyLambdaFunction"
-  arn       = aws_lambda_function.whatsapp_scheduler.arn
+  target {
+    arn      = aws_lambda_function.whatsapp_scheduler.arn
+    role_arn = aws_iam_role.whatsapp_scheduler_scheduler.arn
+  }
 }
--- infrastructure/whatsapp_scheduler/lambda.tf
+++ infrastructure/whatsapp_scheduler/lambda.tf
@@ -57,13 +57,44 @@ resource "aws_iam_role_policy_attachment" "whatsapp_scheduler" {
   policy_arn = aws_iam_policy.whatsapp_scheduler.arn
 }
 
+resource "aws_iam_role" "whatsapp_scheduler_scheduler" {
+  name = "whatsapp_scheduler_scheduler"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
-resource "aws_lambda_permission" "allow_cloudwatch" {
-  statement_id  = "AllowExecutionFromCloudWatch"
+resource "aws_iam_role_policy" "whatsapp_scheduler_scheduler" {
+  role = aws_iam_role.whatsapp_scheduler_scheduler.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "lambda:InvokeFunction"
+        ]
+        Resource = aws_lambda_function.whatsapp_scheduler.arn
+      }
+    ]
+  })
+}
+
+
+resource "aws_lambda_permission" "allow_scheduler" {
+  statement_id  = "AllowExecutionFromEventBridgeScheduler"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.whatsapp_scheduler.function_name
-  principal     = "events.amazonaws.com"
+  principal     = "scheduler.amazonaws.com"
 
-  # Reference the ARN of the CloudWatch event rule
-  source_arn = aws_cloudwatch_event_rule.whatsapp_scheduler.arn
+  source_arn = aws_scheduler_schedule.whatsapp_scheduler.arn
 }
